53ec5de624963ecaee0f345e350b6b67b2fb726e
vickybaby123 <127369948+vickybaby123@users.noreply.github.com>
2026-05-24 19:23:30 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
 import os
 import sys
 
-# Vercel 运行时 /var/task 是项目根目录
 sys.path.insert(0, "/var/task")
 
-from backend.main import app
+from main import app
 from mangum import Mangum
 
 handler = Mangum(app, lifespan="off")
